5b0de91377f84b869a487abd922623f9b9e63f26
carullahtursun <62027425+carullahtursun@users.noreply.github.com>
2022-12-06 17:22:12 +0300

    2.

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,14 @@
 
 var name =prompt("isminizi girin : ");
 
+var days=["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","pazar"];
+
 document.getElementById('myName').innerHTML=name;
 let a;
     let time;
     setInterval(() => {
       a = new Date();
-      time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+      time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds() +' '+ days[a.getDay()-1] ;
       document.getElementById('myClock').innerHTML = time;
     }, 1000);
 
